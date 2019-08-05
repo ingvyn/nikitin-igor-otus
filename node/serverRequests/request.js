0bd9c0c1d086380d0deb6269adb97f809ffbb202
ingvyn <34935122+ingvyn@users.noreply.github.com>
2019-08-05 23:19:06 +0300

    Update request.js

--- node/serverRequests/request.js
+++ node/serverRequests/request.js
@@ -1,23 +1,19 @@
 
 let http = require('http');
+let program = require('commander');
+program
+  .option('-l, --limit <requestLimit>', 'Define number of requests')
+  .option('-t, --type <requestType>', 'Define type of requests: serial or parallel')
+  .parse(process.argv);
 
-let requestLimit = (process.argv[2] === undefined) ? 0 : process.argv[2];
-let requestType = process.argv[3];
-if (requestType === "parallel") {
-  parallelRequests (requestLimit)
+let requestOptions = {
+  host:'127.0.0.1',
+  port: 3000,
+  path:'/'
 }
-  else if (requestType === "serial") {
-    serialRequests (requestLimit)
-  }
-    else throw new ReferenceError ('Second argument must be of type "serial" or "parallel"');
-
-function serialRequests(counter) {
+const serialRequests = (counter) => {
   if (counter) {
-    http.request({
-      host:'127.0.0.1',
-      port: 3000,
-      path:'/'
-      }, res => {
+    http.request(requestOptions, res => {
         console.log(res.statusCode);
         serialRequests(--counter);
       }
@@ -25,15 +21,29 @@ function serialRequests(counter) {
   } else return;
 }
 
-function parallelRequests (counter) {
+const parallelRequests = (counter) => {
   for (var i=0; i < counter; i++) {
-    http.request({
-        host:'127.0.0.1',
-        port: 3000,
-        path:'/'
-    }, res => {
+    http.request(requestOptions, res => {
         console.log(res.statusCode);
         }
     ).end();
   }
  }
+ 
+ const init = (requestLimit, requestType) => {
+  if  (!/^\d+$/.test(requestLimit))
+    throw new TypeError ('Limit of request must be defined as number');
+  if (requestType === "parallel") {
+    parallelRequests (requestLimit)
+  }
+    else if (requestType === "serial") {
+      serialRequests (requestLimit)
+    }
+      else throw new TypeError ('Type of request must be "serial" or "parallel"');
+}
+
+init(program.limit, program.type);
+
+
+
+
